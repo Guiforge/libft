45bd88cb9d90f39c3f741dbdf4ac0510d87244f7
Guillaume Pouyat <guiforge@gmail.com>
2017-06-16 15:40:14 +0200

    [UPDATE]: ft_str_is

--- libft_srcs/string/is/ft_str_isdigit.c
+++ libft_srcs/string/is/ft_str_isdigit.c
@@ -6,13 +6,13 @@
 /*   By: gpouyat <gpouyat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/06 14:56:46 by gpouyat           #+#    #+#             */
-/*   Updated: 2017/03/06 14:57:02 by gpouyat          ###   ########.fr       */
+/*   Updated: 2017/06/16 14:26:28 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_is_number(char *s)
+int		ft_str_isdigit(char *s)
 {
 	int	i;
 	int	ret;
