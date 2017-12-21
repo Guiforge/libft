5fabb62c938b9da88c4850bea66e1acbb137b1d6
gpouyat <guiforge@gmail.com>
2017-12-21 19:59:06 +0100

    Add new function

--- sources/libft_srcs/error/error.c
+++ sources/libft_srcs/error/error.c
@@ -6,11 +6,13 @@
 /*   By: gpouyat <gpouyat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/30 21:28:40 by gpouyat           #+#    #+#             */
-/*   Updated: 2017/06/11 13:04:40 by gpouyat          ###   ########.fr       */
+/*   Updated: 2017/12/21 19:39:00 by guiforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <sys/types.h>
+#include <signal.h>
 
 void	basicerror(char *name, char *error, int ex)
 {
@@ -25,4 +27,16 @@ void	basicerror_out(char *name, char *error, int nb)
 	ft_putstr_fd(name, 2);
 	perror(error);
 	exit(nb);
+}
+
+void	exit_error(const char *progname, const char *error, pid_t pid, int sig)
+{
+	ft_putstr_fd(progname, STDERR_FILENO);
+	ft_putstr_fd(": EXIT-ERROR: ", STDERR_FILENO);
+	if (error)
+		ft_putendl_fd(error, STDERR_FILENO);
+	else
+		ft_putendl_fd("", STDERR_FILENO);
+	kill(pid, sig);
+	exit(EXIT_FAILURE);
 }
