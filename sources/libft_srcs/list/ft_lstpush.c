3161d804f6c2c82a1ee3f77bf31ca5804b910243
gpouyat <gpouyat@vejja.fr>
2018-05-10 14:14:39 +0200

    Makefile missing !

--- sources/libft_srcs/list/ft_lstpush.c
+++ sources/libft_srcs/list/ft_lstpush.c
@@ -6,7 +6,7 @@
 /*   By: gpouyat <gpouyat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/10 13:20:28 by gpouyat           #+#    #+#             */
-/*   Updated: 2018/05/10 14:08:49 by gpouyat          ###   ########.fr       */
+/*   Updated: 2018/05/10 14:14:22 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 void	ft_lstpush(t_list *alst, t_list *new)
 {
-	t_list	*temp;
-
-
 	while (alst && alst->next)
 		alst = alst->next;
 	alst->next = new;
