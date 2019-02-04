ca3a4c62638787e810d58a7f17835b47b2e57e7d
Guillaume Pouyat <guiforge@gmail.com>
2019-02-04 17:13:56 +0100

    [FIX] Master: fix ft_strlen_max

--- sources/libft_srcs/string/ft_strlen.c
+++ sources/libft_srcs/string/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: gpouyat <gpouyat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/02 21:22:16 by gpouyat           #+#    #+#             */
-/*   Updated: 2019/02/01 16:45:39 by gpouyat          ###   ########.fr       */
+/*   Updated: 2019/02/04 15:40:54 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ size_t	ft_strlen_max(const char *str, size_t max)
 	size_t		i;
 
 	i = 0;
-	while (str && str[i] && i <= max)
+	while (str && str[i] && i < max)
 		i = i + 1;
 	return (i);
 }
